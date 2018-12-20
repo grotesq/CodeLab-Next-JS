6a6fbe2bb13336444929a06885647c1336797aec
Unknown GrotesQ <unknown@grotesq.com>
2018-12-21 07:35:36 +0900

    Delete firebase.js

--- tiny-sns-cra-spa/src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAsTjEKPjeQfvnXoutXnb9sO0pVNk-1H2E',
-  authDomain: 'tiny-sns.firebaseapp.com',
-  databaseURL: 'https://tiny-sns.firebaseio.com',
-  projectId: 'tiny-sns',
-  storageBucket: 'tiny-sns.appspot.com',
-  messagingSenderId: '1044315402205',
-};
